4e68e5af02d6ed28c0702a4c258a5465835432b3
ShadowPlayer34 <61494312+ShadowPlayer34@users.noreply.github.com>
2022-03-28 09:28:39 +0300

    Initial Commit

---
+++ TMOIS 2(2) s2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TMOIS 2(2) s2
+//
+//  Created by Андрей Худик on 28.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
